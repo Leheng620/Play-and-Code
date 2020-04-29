d4d7585299909dc5680190f07f84a87d9b721090
Leheng620 <llh238704688@gmail.com>
2020-04-30 05:47:12 +0800

    initializing project, create desktop windows and set up the basic properties such as window size. Also include a background image

---
+++ src/Component/Playground.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+export class Playground extends Component {
+    render() {
+        return (
+            <div className='playground-container' style={style}>
+                
+            </div>
+        )
+    }
+}
+
+const style = {
+    heigth: '100%',
+    width: '100%',
+    border: 'solid',
+}
+export default Playground
